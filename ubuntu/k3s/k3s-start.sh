51dd8ff33db8f50e260738028e80a5da7ffb3785
coutug <coutug18@hotmail.com>
2023-08-08 13:53:25 -0400

    update config

--- ubuntu/k3s/k3s-start.sh
+++ ubuntu/k3s/k3s-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash +x
 
-# INSTALL_K3S_EXEC="agent --server https://10.220.118.7:6443 --token token"
+INSTALL_K3S_EXEC="agent --server https://10.220.118.17:6443 --token token"
 INSTALL_K3S_EXEC="server --token token"
 
 /usr/local/bin/k3s $INSTALL_K3S_EXEC
